b820db7dc31a7ed1a022ea2235e007b5966d2171
masa-dot <masato2002918@icloud.com>
2021-07-24 09:35:49 +0900

    a

--- chemigatya.js
+++ chemigatya.js
@@ -9,7 +9,7 @@
     function gatya() {
         if (a == 0) {
             alert("H2");
-            document.write('<img src="photo\\H2.png" alt="" width="500" height="500">')
+            document.write('<img src="photo\\H2.png" width="500" height="500" border="10">')
             document.write('<button id="home" class="btn btn-primary" type="submit">ホームに戻る</button>')
             document.getElementById("home")
             .addEventListener("click", function () {
@@ -20,7 +20,7 @@
 
         else if (a == 1) {
             alert("O2");
-            document.write('<img src="photo\\O2.png" alt="" width="500" height="500">');
+            document.write('<img src="photo\\O2.png" alt="" width="500" height="500" border="10">');
             document.write('<button id="home" class="btn btn-primary" type="submit">ホームに戻る</button>')
             document.getElementById("home")
             .addEventListener("click", function () {
@@ -30,7 +30,7 @@
 
         else if (a == 2) {
             alert("Cl2");
-            document.write('<img src="photo\\.png" alt="" width="500" height="500">');
+            document.write('<img src="photo\\.png" alt="" width="500" height="500" border="10">');
             document.write('<button id="home" class="btn btn-primary" type="submit">ホームに戻る</button>')
             document.getElementById("home")
             .addEventListener("click", function () {
@@ -40,7 +40,7 @@
 
         else if (a == 3){
             alert("F2");
-            document.write('<img src="photo\\F2.png" alt="" width="500" height="500">');
+            document.write('<img src="photo\\F2.png" alt="" width="500" height="500" border="10">');
             document.write('<button id="home" class="btn btn-primary" type="submit">ホームに戻る</button>')
             document.getElementById("home")
             .addEventListener("click", function () {
@@ -50,7 +50,7 @@
 
         else if (a == 4) {
             alert("W");
-            document.write('<img src="photo\\W.png" alt="" width="500" height="500">');
+            document.write('<img src="photo\\W.png" alt="" width="500" height="500" border="10">');
             document.write('<button id="home" class="btn btn-primary" type="submit">ホームに戻る</button>')
             document.getElementById("home")
             .addEventListener("click", function () {
