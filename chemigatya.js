9bfd27884ea22e3c379f210ca976ff912496001f
masa-dot <masato2002918@icloud.com>
2021-08-25 21:20:06 +0900

    He

--- chemigatya.js
+++ chemigatya.js
@@ -2,17 +2,15 @@
         return Math.floor(Math.random() * max);
     }
 
-    let group = ['H2', 'O2', 'Cl2', 'F2', 'W'];
+    let group = ['H2', 'O2', 'Cl2', 'F2', 'W','He'];
     let a = getRandomInt(group.length);
 
 //ガチャを回す     
     function gatya() {
-        let h,o,cl,f,w
+        let h,o,cl,f,w,he
         if (a == 0) {
             alert("H2");
             localStorage.setItem('h','true')
-            localStorage.getItem('h')
-            console.log(h)
             document.write('<img src="photo\\H2.png" width="500" height="500" border="10">')
             document.write('<button id="home" class="btn btn-primary" type="submit">ホームに戻る</button>')
             document.getElementById("home")
@@ -24,8 +22,6 @@
         else if (a == 1) {
             alert("O2");
             localStorage.setItem('o','true')
-            localStorage.getItem('o')
-            console.log(o);
             document.write('<img src="photo\\O2.png" alt="" width="500" height="500" border="10">');
             document.write('<button id="home" class="btn btn-primary" type="submit">ホームに戻る</button>')
             document.getElementById("home")
@@ -37,8 +33,6 @@
         else if (a == 2) {
             alert("Cl2");
             localStorage.setItem('cl','true')
-            localStorage.getItem('cl')
-            console.log(cl);
             document.write('<img src="photo\\Cl2.png" alt="" width="500" height="500" border="10">');
             document.write('<button id="home" class="btn btn-primary" type="submit">ホームに戻る</button>')
             document.getElementById("home")
@@ -50,8 +44,7 @@
         else if (a == 3){
             alert("F2");
             localStorage.setItem('f','true')
-            localStorage.getItem('f')
-            console.log(f);
+
             document.write('<img src="photo\\F2.png" alt="" width="500" height="500" border="10">');
             document.write('<button id="home" class="btn btn-primary" type="submit">ホームに戻る</button>')
             document.getElementById("home")
@@ -64,7 +57,6 @@
             alert("W");
             localStorage.setItem('w','true')
             localStorage.getItem('w')
-            console.log(w);
             document.write('<img src="photo\\W.png" alt="" width="500" height="500" border="10">');
             document.write('<button id="home" class="btn btn-primary" type="submit">ホームに戻る</button>')
             document.getElementById("home")
@@ -72,6 +64,17 @@
             location.replace("https://hu-hicoder.github.io/ChemiGatya/");
             }, false);
         }
+        else if(a == 5){
+            alert("He");
+            localStorage.setItem('he','true')
+            document.write('<img src="photo\\He.png" alt="" width="500" height="500" border="10">');
+            document.write('<button id="home" class="btn btn-primary" type="submit">ホームに戻る</button>')
+            document.getElementById("home")
+            .addEventListener("click", function () {
+            location.replace("https://hu-hicoder.github.io/ChemiGatya/");
+            }, false);
+        }
+
     }
     
     const el = document.getElementById("push");
