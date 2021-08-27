685b72e5cffe814c1ad7310640e9c33b4be67e96
masa-dot <masato2002918@icloud.com>
2021-08-27 16:11:41 +0900

    a

---
+++ galary.js
@@ -0,0 +1,340 @@
+let H = localStorage.getItem('h');
+let He = localStorage.getItem('he');
+let Li = localStorage.getItem('li');
+let Be = localStorage.getItem('be');
+let B = localStorage.getItem('b');
+let C = localStorage.getItem('c');
+let N = localStorage.getItem('n');
+let O = localStorage.getItem('o');
+let F = localStorage.getItem('f');
+let Ne = localStorage.getItem('ne');
+let Na = localStorage.getItem('na');
+let Mg = localStorage.getItem('mg');
+let Al = localStorage.getItem('al');
+let Si = localStorage.getItem('si');
+let P = localStorage.getItem('p');
+let S = localStorage.getItem('s');
+let Cl = localStorage.getItem('cl');
+let Ar = localStorage.getItem('ar');
+let K = localStorage.getItem('k');
+let Ca = localStorage.getItem('ca');
+let Sc = localStorage.getItem('sc');
+let Ti = localStorage.getItem('ti');
+let V = localStorage.getItem('v');
+let Cr = localStorage.getItem('cr');
+let Mn = localStorage.getItem('mn');
+let Fe = localStorage.getItem('fe');
+let Co = localStorage.getItem('co');
+let Ni = localStorage.getItem('ni');
+let Cu = localStorage.getItem('cu');
+let Zn = localStorage.getItem('zn')    
+let Ga = localStorage.getItem('ga');
+let Ge = localStorage.getItem('ge');
+let As = localStorage.getItem('as');
+let Se = localStorage.getItem('se');
+let Br = localStorage.getItem('br');
+let Kr = localStorage.getItem('kr');
+
+
+let W = localStorage.getItem('w');
+
+
+    if (H === 'true') {
+        let imgH = document.getElementById("imgH");
+        imgH.src = "photo/H2.png";
+    }
+    else{
+        let imgH = document.getElementById("imgH");
+        imgH.src = "element/H.JPG";
+    }
+    if (He === 'true') {
+        let imgHe = document.getElementById("imgHe");
+        imgHe.src = "photo/He.png";
+    }
+    else{
+        let imgHe = document.getElementById("imgHe");
+        imgHe.src = "element/He.JPG";
+    }
+    if (Li === 'true') {
+        let imgLi = document.getElementById("imgLi");
+        imgLi.src = "photo/Li.png";
+    }
+    else{
+        let imgLi = document.getElementById("imgLi");
+        imgLi.src = "element/Li.JPG";
+    }
+    if (Be === 'true') {
+        let imgBe = document.getElementById("imgBe");
+        imgBe.src = "photo/Be.png";
+    }
+    else{
+        let imgBe = document.getElementById("imgBe");
+        imgBe.src = "element/Be.JPG";
+    }
+    if (B === 'true') {
+        let imgB = document.getElementById("imgB");
+        imgB.src = "photo/B.png";
+    }
+    else{
+        let imgB = document.getElementById("imgB");
+        imgB.src = "element/B.JPG";
+    }
+    if (C === 'true') {
+        let imgC = document.getElementById("imgC");
+        imgC.src = "photo/C.png";
+    }
+    else{
+        let imgC = document.getElementById("imgC");
+        imgC.src = "element/C.JPG";
+    }
+    if (N === 'true') {
+        let imgN = document.getElementById("imgN");
+        imgN.src = "photo/N.png";
+    }
+    else{
+        let imgB = document.getElementById("imgN");
+        imgB.src = "element/N.JPG";
+    }
+    if (O === 'true') {
+        let imgO = document.getElementById("imgO");
+        imgO.src = "photo/O2.png";
+    }
+    else{
+        let imgO = document.getElementById("imgO");
+        imgO.src = "element/O.JPG";
+    }
+    if (F === 'true') {
+        let imgF = document.getElementById("imgF");
+        imgF.src = "photo/F2.png";
+    }
+    else{
+        let imgF = document.getElementById("imgF");
+        imgF.src = "element/F.JPG";
+    }
+    if (Ne === 'true') {
+        let imgNe = document.getElementById("imgNe");
+        imgNe.src = "photo/Ne.png";
+    }
+    else{
+        let imgNe = document.getElementById("imgNe");
+        imgNe.src = "element/Ne.JPG";
+    }
+    if (Na === 'true') {
+        let imgNa = document.getElementById("imgNa");
+        imgNa.src = "photo/Na.png";
+    }
+    else{
+        let imgNa = document.getElementById("imgNa");
+        imgNa.src = "element/Na.JPG";
+    }
+    if (Mg === 'true') {
+        let imgMg = document.getElementById("imgMg");
+        imgMg.src = "photo/Mg.png";
+    }
+    else{
+        let imgMg = document.getElementById("imgMg");
+        imgMg.src = "element/Mg.JPG";
+    }
+    if (Al === 'true') {
+        let imgAl = document.getElementById("imgAl");
+        imgAl.src = "photo/Al.png";
+    }
+    else{
+        let imgAl = document.getElementById("imgAl");
+        imgAl.src = "element/Al.JPG";
+    }
+    if (Si === 'true') {
+        let imgSi = document.getElementById("imgSi");
+        imgSi.src = "photo/Si.png";
+    }
+    else{
+        let imgSi = document.getElementById("imgSi");
+        imgSi.src = "element/Si.JPG";
+    }
+    if (P === 'true') {
+        let imgP = document.getElementById("imgP");
+        imgP.src = "photo/P.png";
+    }
+    else{
+        let imgP = document.getElementById("imgP");
+        imgP.src = "element/P.JPG";
+    }
+    if (S === 'true') {
+        let imgS = document.getElementById("imgS");
+        imgS.src = "photo/S.png";
+    }
+    else{
+        let imgS = document.getElementById("imgS");
+        imgS.src = "element/S.JPG";
+    }
+    if (Cl === 'true') {
+        let imgCl = document.getElementById("imgCl");
+        imgCl.src = "photo/Cl2.png";
+    }
+    else{
+        let imgCl = document.getElementById("imgCl");
+        imgCl.src = "element/Cl.JPG";
+    }
+    if (Ar === 'true') {
+        let imgAr = document.getElementById("imgAr");
+        imgAr.src = "photo/Ar.png";
+    }
+    else{
+        let imgAr = document.getElementById("imgAr");
+        imgAr.src = "element/Ar.JPG";
+    }
+    if (K === 'true') {
+        let imgK = document.getElementById("imgK");
+        imgK.src = "photo/K.png";
+    }
+    else{
+        let imgK = document.getElementById("imgK");
+        imgK.src = "element/K.JPG";
+    }
+    if (Ca === 'true') {
+        let imgCa = document.getElementById("imgCa");
+        imgCa.src = "photo/Ca.png";
+    }
+    else{
+        let imgCa = document.getElementById("imgCa");
+        imgCa.src = "element/Ca.JPG";
+    }
+    if (Sc === 'true') {
+        let imgSc = document.getElementById("imgSc");
+        imgSc.src = "photo/Sc.png";
+    }
+    else{
+        let imgSc = document.getElementById("imgSc");
+        imgSc.src = "element/Sc.JPG";
+    }
+    if  (Ti === 'true') {
+        let imgTi = document.getElementById("imgTi");
+        imgTi.src = "photo/Ti.png";
+    }
+    else{
+        let imgTi = document.getElementById("imgTi");
+        imgTi.src = "element/Ti.JPG";
+    }
+    if (V === 'true') {
+        let imgV = document.getElementById("imgV");
+        imgV.src = "photo/V.png";
+    }
+    else{
+        let imgV = document.getElementById("imgV");
+        imgV.src = "element/V.JPG";
+    }
+    if (Cr === 'true') {
+        let imgCr = document.getElementById("imgCr");
+        imgCr.src = "photo/Cr.png";
+    }
+    else{
+        let imgCr = document.getElementById("imgCr");
+        imgCr.src = "element/Cr.JPG";
+    }
+    if (Mn === 'true') {
+        let imgMn = document.getElementById("imgMn");
+        imgMn.src = "photo/Mn.png";
+    }
+    else{
+        let imgMn = document.getElementById("imgMn");
+        imgMn.src = "element/Mn.JPG";
+    }
+    if (Fe === 'true') {
+        let imgFe = document.getElementById("imgFe");
+        imgFe.src = "photo/Fe.png";
+    }
+    else{
+        let imgFe = document.getElementById("imgFe");
+        imgFe.src = "element/Fe.JPG";
+    }
+    if (Co === 'true') {
+        let imgCo = document.getElementById("imgCo");
+        imgCo.src = "photo/Co.png";
+    }
+    else{
+        let imgCo = document.getElementById("imgCo");
+        imgCo.src = "element/Co.JPG";
+    }
+    if (Ni === 'true') {
+        let imgNi = document.getElementById("imgNi");
+        imgNi.src = "photo/Ni.png";
+    }
+    else{
+        let imgNi = document.getElementById("imgNi");
+        imgNi.src = "element/Ni.JPG";
+    }
+    if (Cu === 'true') {
+        let imgCu = document.getElementById("imgCu");
+        imgCu.src = "photo/Cu.png";
+    }
+    else{
+        let imgCu = document.getElementById("imgCu");
+        imgCu.src = "element/Cu.JPG";
+    }
+
+    if (Zn === 'true') {
+        let imgZn = document.getElementById("imgZn");
+        imgZn.src = "photo/Zn.png";
+    }
+    else{
+        let imgZn = document.getElementById("imgZn");
+        imgZn.src = "element/Zn.JPG";
+    }
+    if (Ga === 'true') {
+        let imgGa = document.getElementById("imgGa");
+        imgGa.src = "photo/Ga.png";
+    }
+    else{
+        let imgGa = document.getElementById("imgGa");
+        imgGa.src = "element/Ga.JPG";
+    }        
+    if (Ge === 'true') {
+        let imgGe = document.getElementById("imgGe");
+        imgGe.src = "photo/Ge.png";
+    }
+    else{
+        let imgGe = document.getElementById("imgGe");
+        imgGe.src = "element/Ge.JPG";
+    }
+    if (As === 'true') {
+        let imgAs = document.getElementById("imgAs");
+        imgAs.src = "photo/As.png";
+    }
+    else{
+        let imgAs = document.getElementById("imgAs");
+        imgAs.src = "element/As.JPG";
+    }
+    if (Se === 'true') {
+        let imgSe = document.getElementById("imgSe");
+        imgSe.src = "photo/Se.png";
+    }
+    else{
+        let imgSe = document.getElementById("imgSe");
+        imgSe.src = "element/Se.JPG";
+    }
+    if (Br === 'true') {
+        let imgBr = document.getElementById("imgBr");
+        imgBr.src = "photo/Br.png";
+    }
+    else{
+        let imgBr = document.getElementById("imgBr");
+        imgBr.src = "element/Br.JPG";
+    }
+    if (Kr === 'true') {
+        let imgKr = document.getElementById("imgKr");
+        imgKr.src = "photo/Kr.png";
+    }
+    else{
+        let imgKr = document.getElementById("imgKr");
+        imgKr.src = "element/Kr.JPG";
+    }
+
+
+    if (W === 'true') {
+        let imgW = document.getElementById("imgW");
+        imgW.src = "photo/W.png";
+    }
+    else{
+        let imgW = document.getElementById("imgW");
+        imgW.src = "element/margin.PNG";
+    }
