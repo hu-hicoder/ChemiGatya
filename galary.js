cec12f6d0a74694192178a92170cb54da6f1625d
masa-dot <masato2002918@icloud.com>
2021-09-03 23:57:27 +0900

    layoutchange

--- galary.js
+++ galary.js
@@ -27,314 +27,308 @@ let Fe = localStorage.getItem('fe');
 let Co = localStorage.getItem('co');
 let Ni = localStorage.getItem('ni');
 let Cu = localStorage.getItem('cu');
-let Zn = localStorage.getItem('zn')    
+let Zn = localStorage.getItem('zn');    
 let Ga = localStorage.getItem('ga');
 let Ge = localStorage.getItem('ge');
 let As = localStorage.getItem('as');
 let Se = localStorage.getItem('se');
 let Br = localStorage.getItem('br');
 let Kr = localStorage.getItem('kr');
-
-
 let W = localStorage.getItem('w');
 
-
+    //ギャラリーでの画像判定
+    let imgH = document.getElementById("imgH");
     if (H === 'true') {
-        let imgH = document.getElementById("imgH");
         imgH.src = "photo/H2.png";
     }
     else{
-        let imgH = document.getElementById("imgH");
         imgH.src = "element/H.JPG";
     }
+
+    let imgHe = document.getElementById("imgHe");
     if (He === 'true') {
-        let imgHe = document.getElementById("imgHe");
         imgHe.src = "photo/He.png";
     }
     else{
-        let imgHe = document.getElementById("imgHe");
         imgHe.src = "element/He.JPG";
     }
+
+    let imgLi = document.getElementById("imgLi");
     if (Li === 'true') {
-        let imgLi = document.getElementById("imgLi");
         imgLi.src = "photo/Li.png";
     }
     else{
-        let imgLi = document.getElementById("imgLi");
         imgLi.src = "element/Li.JPG";
     }
+
+    let imgBe = document.getElementById("imgBe");
     if (Be === 'true') {
-        let imgBe = document.getElementById("imgBe");
         imgBe.src = "photo/Be.png";
     }
     else{
-        let imgBe = document.getElementById("imgBe");
         imgBe.src = "element/Be.JPG";
     }
+
+    let imgB = document.getElementById("imgB");
     if (B === 'true') {
-        let imgB = document.getElementById("imgB");
         imgB.src = "photo/B.png";
     }
     else{
-        let imgB = document.getElementById("imgB");
         imgB.src = "element/B.JPG";
     }
+
+    let imgC = document.getElementById("imgC");
     if (C === 'true') {
-        let imgC = document.getElementById("imgC");
         imgC.src = "photo/C.png";
     }
     else{
-        let imgC = document.getElementById("imgC");
         imgC.src = "element/C.JPG";
     }
+
+    let imgN = document.getElementById("imgN");
     if (N === 'true') {
-        let imgN = document.getElementById("imgN");
         imgN.src = "photo/N.png";
     }
     else{
-        let imgB = document.getElementById("imgN");
-        imgB.src = "element/N.JPG";
+        imgN.src = "element/N.JPG";
     }
+
+    let imgO = document.getElementById("imgO");
     if (O === 'true') {
-        let imgO = document.getElementById("imgO");
         imgO.src = "photo/O2.png";
     }
     else{
-        let imgO = document.getElementById("imgO");
         imgO.src = "element/O.JPG";
     }
+
+    let imgF = document.getElementById("imgF");
     if (F === 'true') {
-        let imgF = document.getElementById("imgF");
         imgF.src = "photo/F2.png";
     }
     else{
-        let imgF = document.getElementById("imgF");
         imgF.src = "element/F.JPG";
     }
+
+    let imgNe = document.getElementById("imgNe");
     if (Ne === 'true') {
-        let imgNe = document.getElementById("imgNe");
         imgNe.src = "photo/Ne.png";
     }
     else{
-        let imgNe = document.getElementById("imgNe");
         imgNe.src = "element/Ne.JPG";
     }
+
+    let imgNa = document.getElementById("imgNa");
     if (Na === 'true') {
-        let imgNa = document.getElementById("imgNa");
         imgNa.src = "photo/Na.png";
     }
     else{
-        let imgNa = document.getElementById("imgNa");
         imgNa.src = "element/Na.JPG";
     }
+
+    let imgMg = document.getElementById("imgMg");
     if (Mg === 'true') {
-        let imgMg = document.getElementById("imgMg");
         imgMg.src = "photo/Mg.png";
     }
     else{
-        let imgMg = document.getElementById("imgMg");
         imgMg.src = "element/Mg.JPG";
     }
+
+    let imgAl = document.getElementById("imgAl");
     if (Al === 'true') {
-        let imgAl = document.getElementById("imgAl");
         imgAl.src = "photo/Al.png";
     }
     else{
-        let imgAl = document.getElementById("imgAl");
         imgAl.src = "element/Al.JPG";
     }
+
+    let imgSi = document.getElementById("imgSi");
     if (Si === 'true') {
-        let imgSi = document.getElementById("imgSi");
         imgSi.src = "photo/Si.png";
     }
     else{
-        let imgSi = document.getElementById("imgSi");
         imgSi.src = "element/Si.JPG";
     }
+
+    let imgP = document.getElementById("imgP");
     if (P === 'true') {
-        let imgP = document.getElementById("imgP");
         imgP.src = "photo/P.png";
     }
     else{
-        let imgP = document.getElementById("imgP");
         imgP.src = "element/P.JPG";
     }
+
+    let imgS = document.getElementById("imgS");
     if (S === 'true') {
-        let imgS = document.getElementById("imgS");
         imgS.src = "photo/S.png";
     }
     else{
-        let imgS = document.getElementById("imgS");
         imgS.src = "element/S.JPG";
     }
+
+    let imgCl = document.getElementById("imgCl");
     if (Cl === 'true') {
-        let imgCl = document.getElementById("imgCl");
         imgCl.src = "photo/Cl2.png";
     }
     else{
-        let imgCl = document.getElementById("imgCl");
         imgCl.src = "element/Cl.JPG";
     }
+
+    let imgAr = document.getElementById("imgAr");
     if (Ar === 'true') {
-        let imgAr = document.getElementById("imgAr");
         imgAr.src = "photo/Ar.png";
     }
     else{
-        let imgAr = document.getElementById("imgAr");
         imgAr.src = "element/Ar.JPG";
     }
+
+    let imgK = document.getElementById("imgK");
     if (K === 'true') {
-        let imgK = document.getElementById("imgK");
         imgK.src = "photo/K.png";
     }
     else{
-        let imgK = document.getElementById("imgK");
         imgK.src = "element/K.JPG";
     }
+
+    let imgCa = document.getElementById("imgCa");
     if (Ca === 'true') {
-        let imgCa = document.getElementById("imgCa");
         imgCa.src = "photo/Ca.png";
     }
     else{
-        let imgCa = document.getElementById("imgCa");
         imgCa.src = "element/Ca.JPG";
     }
+
+    let imgSc = document.getElementById("imgSc");
     if (Sc === 'true') {
-        let imgSc = document.getElementById("imgSc");
         imgSc.src = "photo/Sc.png";
     }
     else{
-        let imgSc = document.getElementById("imgSc");
         imgSc.src = "element/Sc.JPG";
     }
+
+    let imgTi = document.getElementById("imgTi");
     if  (Ti === 'true') {
-        let imgTi = document.getElementById("imgTi");
         imgTi.src = "photo/Ti.png";
     }
     else{
-        let imgTi = document.getElementById("imgTi");
         imgTi.src = "element/Ti.JPG";
     }
+
+    let imgV = document.getElementById("imgV");
     if (V === 'true') {
-        let imgV = document.getElementById("imgV");
         imgV.src = "photo/V.png";
     }
     else{
-        let imgV = document.getElementById("imgV");
         imgV.src = "element/V.JPG";
     }
+
+    let imgCr = document.getElementById("imgCr");
     if (Cr === 'true') {
-        let imgCr = document.getElementById("imgCr");
         imgCr.src = "photo/Cr.png";
     }
     else{
-        let imgCr = document.getElementById("imgCr");
         imgCr.src = "element/Cr.JPG";
     }
+
+    let imgMn = document.getElementById("imgMn");
     if (Mn === 'true') {
-        let imgMn = document.getElementById("imgMn");
         imgMn.src = "photo/Mn.png";
     }
     else{
-        let imgMn = document.getElementById("imgMn");
         imgMn.src = "element/Mn.JPG";
     }
+
+    let imgFe = document.getElementById("imgFe");
     if (Fe === 'true') {
-        let imgFe = document.getElementById("imgFe");
         imgFe.src = "photo/Fe.png";
     }
     else{
-        let imgFe = document.getElementById("imgFe");
         imgFe.src = "element/Fe.JPG";
     }
+
+    let imgCo = document.getElementById("imgCo");
     if (Co === 'true') {
-        let imgCo = document.getElementById("imgCo");
         imgCo.src = "photo/Co.png";
     }
     else{
-        let imgCo = document.getElementById("imgCo");
         imgCo.src = "element/Co.JPG";
     }
+
+    let imgNi = document.getElementById("imgNi");
     if (Ni === 'true') {
-        let imgNi = document.getElementById("imgNi");
         imgNi.src = "photo/Ni.png";
     }
     else{
-        let imgNi = document.getElementById("imgNi");
         imgNi.src = "element/Ni.JPG";
     }
+
+    let imgCu = document.getElementById("imgCu");
     if (Cu === 'true') {
-        let imgCu = document.getElementById("imgCu");
         imgCu.src = "photo/Cu.png";
     }
     else{
-        let imgCu = document.getElementById("imgCu");
         imgCu.src = "element/Cu.JPG";
     }
 
+    let imgZn = document.getElementById("imgZn");
     if (Zn === 'true') {
-        let imgZn = document.getElementById("imgZn");
         imgZn.src = "photo/Zn.png";
     }
     else{
-        let imgZn = document.getElementById("imgZn");
         imgZn.src = "element/Zn.JPG";
     }
+
+    let imgGa = document.getElementById("imgGa");
     if (Ga === 'true') {
-        let imgGa = document.getElementById("imgGa");
         imgGa.src = "photo/Ga.png";
     }
     else{
-        let imgGa = document.getElementById("imgGa");
         imgGa.src = "element/Ga.JPG";
-    }        
+    }   
+    
+    let imgGe = document.getElementById("imgGe");
     if (Ge === 'true') {
-        let imgGe = document.getElementById("imgGe");
         imgGe.src = "photo/Ge.png";
     }
     else{
-        let imgGe = document.getElementById("imgGe");
         imgGe.src = "element/Ge.JPG";
     }
+
+    let imgAs = document.getElementById("imgAs");
     if (As === 'true') {
-        let imgAs = document.getElementById("imgAs");
         imgAs.src = "photo/As.png";
     }
     else{
-        let imgAs = document.getElementById("imgAs");
         imgAs.src = "element/As.JPG";
     }
+
+    let imgSe = document.getElementById("imgSe");
     if (Se === 'true') {
-        let imgSe = document.getElementById("imgSe");
         imgSe.src = "photo/Se.png";
     }
     else{
-        let imgSe = document.getElementById("imgSe");
         imgSe.src = "element/Se.JPG";
     }
+
+    let imgBr = document.getElementById("imgBr");
     if (Br === 'true') {
-        let imgBr = document.getElementById("imgBr");
         imgBr.src = "photo/Br.png";
     }
     else{
-        let imgBr = document.getElementById("imgBr");
         imgBr.src = "element/Br.JPG";
     }
+
+    let imgKr = document.getElementById("imgKr");
     if (Kr === 'true') {
-        let imgKr = document.getElementById("imgKr");
         imgKr.src = "photo/Kr.png";
     }
     else{
-        let imgKr = document.getElementById("imgKr");
         imgKr.src = "element/Kr.JPG";
     }
 
-
+    let imgW = document.getElementById("imgW");
     if (W === 'true') {
-        let imgW = document.getElementById("imgW");
         imgW.src = "photo/W.png";
     }
     else{
-        let imgW = document.getElementById("imgW");
         imgW.src = "element/margin.PNG";
     }
---
+++ pick.js
@@ -0,0 +1,298 @@
+let img = document.getElementById("pick");
+
+function pickh(){
+    if (H === 'true') {
+        img.src = "photo/H2.png";
+    }
+    else {
+        img.src = "element/H.JPG";
+    }
+}
+function pickhe(){
+    if (He === 'true') {
+        img.src = "photo/He.png";
+    }
+    else {
+        img.src = "element/He.JPG";
+    }
+}
+function pickli(){
+    if (Li === 'true') {
+        img.src = "photo/Li.png";
+    }
+    else {
+        img.src = "element/Li.JPG";
+    }
+}
+function pickbe(){
+    if (Be === 'true') {
+        img.src = "photo/Be.png";
+    }
+    else {
+        img.src = "element/Be.JPG";
+    }
+}
+function pickb(){
+    if (B === 'true') {
+        img.src = "photo/B.png";
+    }
+    else {
+        img.src = "element/B.JPG";
+    }
+}
+function pickc(){
+    if (C === 'true') {
+        img.src = "photo/C.png";
+    }
+    else {
+        img.src = "element/C.JPG";
+    }
+}
+function pickn(){
+    if (N === 'true') {
+        img.src = "photo/N.png";
+    }
+    else {
+        img.src = "element/N.JPG";
+    }
+}
+function picko(){
+    if (O === 'true') {
+        img.src = "photo/O2.png";
+    }
+    else {
+        img.src = "element/O.JPG";
+    }
+}
+function pickf(){
+    if (F === 'true') {
+        img.src = "photo/F2.png";
+    }
+    else {
+        img.src = "element/F.JPG";
+    }
+}
+function pickne(){
+    if (Ne === 'true') {
+        img.src = "photo/Ne.png";
+    }
+    else {
+        img.src = "element/Ne.JPG";
+    }
+}
+function pickna(){
+    if (Na === 'true') {
+        img.src = "photo/Na.png";
+    }
+    else {
+        img.src = "element/Na.JPG";
+    }
+}
+function pickmg(){
+    if (Mg === 'true') {
+        img.src = "photo/Mg.png";
+    }
+    else {
+        img.src = "element/Mg.JPG";
+    }
+}
+function pickal(){
+    if (Al === 'true') {
+        img.src = "photo/Al.png";
+    }
+    else {
+        img.src = "element/Al.JPG";
+    }
+}
+function picksi(){
+    if (Si === 'true') {
+        img.src = "photo/Si.png";
+    }
+    else {
+        img.src = "element/Si.JPG";
+    }
+}
+function pickp(){
+    if (P === 'true') {
+        img.src = "photo/P.png";
+    }
+    else {
+        img.src = "element/P.JPG";
+    }
+}
+function picks(){
+    if (S === 'true') {
+        img.src = "photo/S.png";
+    }
+    else {
+        img.src = "element/S.JPG";
+    }
+}
+function pickcl(){
+    if (Cl === 'true') {
+        img.src = "photo/Cl2.png";
+    }
+    else {
+        img.src = "element/Cl.JPG";
+    }
+}
+function pickar(){
+    if (Ar === 'true') {
+        img.src = "photo/Ar.png";
+    }
+    else {
+        img.src = "element/Ar.JPG";
+    }
+}
+function pickk(){
+    if (K === 'true') {
+        img.src = "photo/K.png";
+    }
+    else {
+        img.src = "element/K.JPG";
+    }
+}
+function pickca(){
+    if (Ca === 'true') {
+        img.src = "photo/Ca.png";
+    }
+    else {
+        img.src = "element/Ca.JPG";
+    }
+}
+function picksc(){
+    if (Sc === 'true') {
+        img.src = "photo/Sc.png";
+    }
+    else {
+        img.src = "element/Sc.JPG";
+    }
+}
+function pickti(){
+    if (Ti === 'true') {
+        img.src = "photo/Ti.png";
+    }
+    else {
+        img.src = "element/Ti.JPG";
+    }
+}
+function pickv(){
+    if (V === 'true') {
+        img.src = "photo/V.png";
+    }
+    else {
+        img.src = "element/V.JPG";
+    }
+}
+function pickcr(){
+    if (Cr === 'true') {
+        img.src = "photo/Cr.png";
+    }
+    else {
+        img.src = "element/Cr.JPG";
+    }
+}
+function pickmn(){
+    if (Mn === 'true') {
+        img.src = "photo/Mn.png";
+    }
+    else {
+        img.src = "element/Mn.JPG";
+    }
+}
+function pickfe(){
+    if (Fe === 'true') {
+        img.src = "photo/Fe.png";
+    }
+    else {
+        img.src = "element/Fe.JPG";
+    }
+}
+function pickco(){
+    if (Co === 'true') {
+        img.src = "photo/Co.png";
+    }
+    else {
+        img.src = "element/Co.JPG";
+    }
+}
+function pickni(){
+    if (Ni === 'true') {
+        img.src = "photo/Ni.png";
+    }
+    else {
+        img.src = "element/Ni.JPG";
+    }
+}
+function pickcu(){
+    if (Cu === 'true') {
+        img.src = "photo/Cu.png";
+    }
+    else {
+        img.src = "element/Cu.JPG";
+    }
+}
+function pickzn(){
+    if (Zn === 'true') {
+        img.src = "photo/Zn.png";
+    }
+    else {
+        img.src = "element/Zn.JPG";
+    }
+}
+function pickga(){
+    if (Ga === 'true') {
+        img.src = "photo/Ga.png";
+    }
+    else {
+        img.src = "element/Ga.JPG";
+    }
+}
+function pickge(){
+    if (Ge === 'true') {
+        img.src = "photo/Ge.png";
+    }
+    else {
+        img.src = "element/Ge.JPG";
+    }
+}
+function pickas(){
+    if (As === 'true') {
+        img.src = "photo/As.png";
+    }
+    else {
+        img.src = "element/As.JPG";
+    }
+}
+function pickse(){
+    if (Se === 'true') {
+        img.src = "photo/Se.png";
+    }
+    else {
+        img.src = "element/Se.JPG";
+    }
+}
+function pickbr(){
+    if (Br === 'true') {
+        img.src = "photo/Br.png";
+    }
+    else {
+        img.src = "element/Br.JPG";
+    }
+}
+function pickkr(){
+    if (Kr === 'true') {
+        img.src = "photo/Kr.png";
+    }
+    else {
+        img.src = "element/Kr.JPG";
+    }
+}
+function pickw(){
+    if (W === 'true') {
+        img.src = "photo/W.png";
+    }
+    else {
+        img.src = "element/margin.PNG";
+    }
+}
